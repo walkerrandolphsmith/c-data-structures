9acfec7b6cdf20b32ca1af35a2865287868c50bd
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-07-27 22:47:15 -0400

    Struct cannot contain funcs.

--- src/LinkedList.c
+++ src/LinkedList.c
@@ -5,10 +5,12 @@
 struct node {
 	int data;
 	struct node *next;
-	bool contains(element) {
-		return data == element || next.contains(element);
-	}
+	//bool (*contains)(struct node *);
 };
+
+//bool node_contains(struct node *self) {
+//	return self->data == 5 || self->next->contains(element);
+//}
 
 struct node *head = NULL;
 
@@ -36,15 +38,19 @@ void add(int element) {
 	}
 }
 
-bool contians(elemet) {
-	if(head == NULL) {
-		return false;
-	} else {
-		return head.contains(element);
-	}
-}
+//bool contians(int element) {
+//	return head == NULL ? false : head->contains(element);
+//}
 
 int main()
 {
+    //struct node n;
+    //node.contains = node_contains;
+    //node.contains(&node);
+
+    printf("Is empty: %s \n", isEmpty()? "true" : "false");
+
+    add(5);
+
     printf("Is empty: %s \n", isEmpty()? "true" : "false");
 }
